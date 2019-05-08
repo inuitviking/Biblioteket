80928140eccfab09a282d0ad4692811df411a478
inuitviking <runurth@gmail.com>
2019-05-08 09:08:35 +0200

    Added summary comments in Bibliotek.cs

--- Classes/Bibliotek.cs
+++ Classes/Bibliotek.cs
@@ -10,41 +10,47 @@ namespace Biblioteket.Classes
 		Laaner laaner;
 		public List<Laaner> laanerList;
 
-		/*
-			This is the class constructor
-		*/
+		/// <summary>
+		/// 	This is the class constructor.
+		/// 	It doesn't return anything, but creates makes sure the
+		/// 	bibliotek object has a navn and also creates a new
+		/// 	list containing all book renters in a list called
+		/// 	laanerList.
+		/// </summary>
+		/// <param name="navn"></param>
 		public Bibliotek(string navn){
 			this.biblioteksNavn = navn;
 			laanerList = new List<Laaner>();
 		} // End of Bibliotek
 
-		/*
-			This method returns a string welcoming the user to the library,
-			to then display the current date. It's simple, really.
-		*/
+		/// <summary>
+		/// 	HentBibliotek returns a string welcoming the user to the library,
+		/// 	to then display the current date. IT's simple, really.
+		/// </summary>
+		/// <returns>Velkomme til {biblioteksNavn} - datoen idag er: {datetime string}</returns>
 		public string HentBibliotek(){
 			string welcome		= "Velkommen til " + this.biblioteksNavn;
 			string currentdate	= " - datoen idag er: " + DateTime.Now.ToString("d/M/yyyy");
 			return welcome + currentdate;
 		} // End of HentBibliotek
 
-		/*
-			This displays laanerNummer, laaner navn, and which library the laaner is
-			subscribed to.
-		*/
+		/// <summary>
+		/// 	HentLaaner displays laanerNummer, laaner navn, and which library the laaner is
+		///		subscribed to.
+		/// 	Currently, this just returns the last value in laanerList
+		/// </summary>
+		/// <returns>Lånernummer: {laanerNummer} - Navn: {navn} er låner hos: {biblioteksNavn}</returns>
 		public string HentLaaner(){
 			return "Lånernummer: " + this.laaner.LaanerNummer
 				+ " - Navn: " + this.laaner.Navn
 				+ " er låner hos: " + this.biblioteksNavn;
 		} // End of HentLaaner
 
-		/*
-			This method gets all items in laanerList.
-			It doesn't quite do as specified in the task (v3.4), which said to
-			"build a long string". I thought this solution below _might_ be a better
-			solution.
-			TODO: Change the comment
-		*/
+		/// <summary>
+		/// 	HentAlleLaanere returns one long string containing all laanere
+		/// </summary>
+		/// <returns>A full list within a single string of all values contained in laanerList</returns>
+
 		public string HentAlleLaanere(){
 			string title = "\n++++ Alle Lånere ++++\n";
 			string bottom = "\n+++++++++++++++++++++";
@@ -54,20 +60,22 @@ namespace Biblioteket.Classes
 			}
 			string result = title + userList + bottom;
 			return result;
-		}
+		}// End of HentAlleLaanere
 
-		/*
-			TODO: CHANGE THIS COMMENT
-			This method creates a laaner. It takes two paramters:
-				1. (string) navn
-				2. (List<Laaner>) laanerList
-
-			It then uses the RandomHex to generate an 8 character
-			hexadecimal string, and then checks if that string
-			exists withing the laanerList. If it does, it
-			continues to loop until it gets a hexadecimal string
-			that doesn't match any laanerNummer in the list.
-		*/
+		/// <summary>
+		/// 	OpretLaaner creates a laaner and returns a string
+		///		containing laanerNummer and navn.
+		///		It uses RandomHex method to generate an 8 character
+		///		hexadecimal string, and then checks if that string
+		///		exists within the laanerList. If it does, it
+		///		continues to loop until it gets a hexadecimal string
+		///		that doesn't match any laanerNummer in the list.
+		///		The unfortunate thing, is that this isn't future
+		///		proof; it can only contain 2048 users, but I think
+		/// 	this is good enough to show an example.
+		/// </summary>
+		/// <param name="navn"></param>
+		/// <returns>Låner oprettet:\n    LaanerID:\t{newHexID}\n    Navn:\t{navn}</returns>
 		public string OpretLaaner(string navn){
 
 			bool laanerExists = true;
@@ -85,12 +93,14 @@ namespace Biblioteket.Classes
 			return $"Låner oprettet:\n    LaanerID:\t{newHexID}\n    Navn:\t{navn}";
 		}// End of OpretLaaner
 
-		/*
-			This method creates a random hexadecimal string in a defined number of characters.
-			It returns a data type "string" containg a hexadecimal value.
-			I've taking it from here:
-			https://stackoverflow.com/a/1054087/3613647
-		*/
+		/// <summary>
+		/// 	RandomHex creates a random hexadecimal string in a defined number of characters.
+		///		It returns a data type "string" containg a hexadecimal value.
+		///		I've taking it from here:
+		///		https://stackoverflow.com/a/1054087/3613647
+		/// </summary>
+		/// <param name="digits"></param>
+		/// <returns>A hexadecimal string of a defined length of characters</returns>
 		private static string RandomHex(int digits){
 
 			Random random = new Random();
